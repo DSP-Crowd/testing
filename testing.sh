5a61d5879c54b4aa0e2d9f7bf9ace7d3d17704c7
Johannes Natter <office@dsp-crowd.com>
2017-07-15 21:27:16 +0000

    add eeprom write / check and test duration

--- testing.sh
+++ testing.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 i2cDir="/sys/class/i2c-adapter/i2c-0"
 i2cAddDevCmd="24c32 0x50"
 i2cDev="0-0050"
+eepFile="rr_base.eep"
 
 echo Testing
 
@@ -14,10 +15,22 @@ while true; do
 	buttonPressed=$(raspi-gpio get 26 | cut -d '=' -f 2 | cut -d ' ' -f 1)
 
 	if [ "${buttonPressed}" -eq "0" ]; then
+		startTime=${SECONDS}
+
 		echo "Button pressed"
 		echo "Starting Tests"
 
-		
+		sudo dd if=${eepFile} of=${i2cDir}/${i2cDev}/eeprom
+		sudo cmp ${eepFile} ${i2cDir}/${i2cDev}/eeprom -n $(wc -c ${eepFile} | cut -d " " -f 1)
+
+		if [ "$?" -eq "0" ]; then
+			echo "Test OK"
+		else
+			echo "Test FAILED"
+		fi
+
+		elapsedTime=$((${SECONDS} - ${startTime}))
+		echo "Duration: ${elapsedTime} Seconds"
 
 		break;
 	fi
